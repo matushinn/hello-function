6667fc9b2e84e7c726dae92c16f9481c4696e8ea
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 09:34:05 +0900

    first commit

---
+++ warikann.py
@@ -0,0 +1,12 @@
+# Table1:1500 3
+
+# Table: 2000 3
+# Table 3647 4
+
+def warikann(amount, number_of_people):
+    print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+
+
+warikann(1500, 3)
+warikann(2000, 3)
+warikann(3647, 3)
