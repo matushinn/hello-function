18f7994e65d68490c5fe84877ff1962e2fba9d6b
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 09:21:04 +0900

    関数の定義の方法を学んだ

---
+++ app.py
@@ -0,0 +1,31 @@
+def hello():
+    print("hello world")
+
+
+hello()
+
+
+def say_hello(name):
+    print(f"Hi {name}")
+
+
+say_hello("Bob")
+
+
+def double(number):
+    return 2 * number
+
+
+double(3)
+
+
+# １分課題
+
+
+def str_combine(name, name1):
+    return "Kazuma Takahashi"
+
+
+result = str_combine("Kazuma", "Takahashi")
+
+print(result)
